a2643fa040b89e1f7885b8d92dbf2f995c913afa
Licko004 <tim.licen@dewesoft.com>
2026-08-25 12:16:56 +0200

    Added pump test

---
+++ vacuum-test.lua
@@ -0,0 +1,25 @@
+
+
+-- SetDO(0, 0, 0, 1)
+
+-- Turning vacuum ON and OFF after 5 seconds
+-- SetToolDO(0, 1, 0, 1)
+-- WaitMs(300)
+-- SetToolDO(0, 0, 0, 1)
+
+-- WaitMs(5000)
+
+-- SetToolDO(0, 1, 0, 1)
+-- WaitMs(300)
+-- SetToolDO(0, 0, 0, 1)
+
+-- Tool DI aquisition test
+function lim_sw_state(io_num)
+    state = GetToolDI(io_num, 0)
+    return state
+end
+
+while(1) do
+    state = lim_sw_state(0)
+    RegisterVar("number", "state")
+end
