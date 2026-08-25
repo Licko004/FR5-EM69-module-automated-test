3fb6e8f49f040acf545d3e6afb68b8bbce16346e
Licko004 <tim.licen@dewesoft.com>
2026-08-25 12:16:37 +0200

    Added degub printing test

---
+++ print-msg-test.lua
@@ -0,0 +1,11 @@
+-- SetUserVal(uservar0, 3)
+
+-- GetUserVal(uservar0)
+
+-- RegisterVar("number", "uservar0") -- uservar0 set to 0 ?
+
+-- SysVar test
+SetSysVarValue(s_var_1, 5)
+
+A = GetSysVarValue(s_var_1)
+RegisterVar("number", "A")
