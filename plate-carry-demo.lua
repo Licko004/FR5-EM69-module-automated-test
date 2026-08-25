cac262dfc332cb975432f5bd4ddcba24c1a96cfb
Licko004 <tim.licen@dewesoft.com>
2026-08-25 12:26:28 +0200

    Succedfully carried 1 module into tray 2

--- plate-carry-demo.lua
+++ plate-carry-demo.lua
@@ -17,6 +17,8 @@ local part_count = 0
 TR1_PITCH_X, TR1_PITCH_Y = 32.8, 56
 TR2_PITCH_X, TR2_PITCH_Y = 32.57, 55.1
 
+TR2_COLS = 8
+TR2_ROWS = 4
 -- Point definitons 
 
 -- Top tray1 (TR1_TOP) is defined with Matrix 1 in webUI
@@ -88,17 +90,33 @@ end
 
 if (switchTripped) then
 
-        part_count = part_count + 1
+    part_count = part_count + 1
 
-        vacuum_toggle() -- turn vacuum ON
-        WaitMs(1000)
+    vacuum_toggle()
+    WaitMs(1000)
 
-        Lin(TR1_TOP, 10, -1, 0, 1, -0.002*m + 56.254*n, 32.803*m + -0.004*n, 30*h + 30*(1+1), 0, 0, 0) -- lift module out of tray
-        PTP(TR1_TOP_OUT, ovl, blendT, offset_flag, 0, 0, 0, 0, 0, 0) -- clear of TR1
-        PTP(TR2_APP, ovl, blendT, offset_flag, 0, 0, 0, 0, 0, 0)     -- approach TR2
+    Lin(TR1_TOP, 10, -1, 0, 1, -0.002*m + 56.254*n, 32.803*m + -0.004*n, 30*h + 30*(1+1), 0, 0, 0) -- lift module out of tray
+    PTP(TR1_TOP_OUT, ovl, blendT, offset_flag, 0, 0, 0, 0, 0, 0) -- clear of TR1
+    PTP(TR2_APP, ovl, blendT, offset_flag, 0, 0, 0, 0, 0, 0)     -- approach TR2
 
-        WaitMs(1000)
-        vacuum_toggle()
+    -- Compute target slot in TR2 from part_count, row-major (fill row, then next row)
+    local idx  = part_count - 1
+    local col2 = idx % TR2_COLS
+    local row2 = (idx - col2) / TR2_COLS   -- integer division without math.floor
+
+    local x2 = col2 * TR2_PITCH_X
+    local y2 = row2 * TR2_PITCH_Y
+
+    Lin(TR2_BTM, 10, -1, 0, 1, x2, y2, 30 + 30, 0, 0, 0)  -- above target slot
+    Lin(TR2_BTM, 10, -1, 0, 1, x2, y2, 30, 0, 0, 0)        -- descend to place height
+
+    WaitMs(500)
+    vacuum_toggle()  -- release part
+    WaitMs(300)
+
+    Lin(TR2_BTM, 10, -1, 0, 1, x2, y2, 30 + 30, 0, 0, 0)  -- retract
+
 end
+
 
 
